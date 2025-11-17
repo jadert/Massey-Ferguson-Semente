2f42e9edd39d01cf0a6dfdc6074113c07f758660
jadert <jaderfeldmann@hotmail.com>
2025-11-17 13:20:14 -0300

    Update app name, launcher color, and version code
    Changed app name to 'MF 40X' in strings.xml, updated launcher background color, and incremented versionCode to 4 in build.gradle.kts. Removed release AAB file and added deviceManager.xml for IDE configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.jadert.mf40x"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
